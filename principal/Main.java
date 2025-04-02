d7095ed6b95e34c036bf842799675e653bd01fbb
GabrielCampos <gabrielfilipecampos1@gmail.com>
2025-04-02 20:15:37 +0000

    Pratica 4

--- principal/Main.java
+++ principal/Main.java
@@ -19,5 +19,23 @@ public class Main {
 
         System.out.println("Elementos do conjunto B:");
         conjunto.imprimirElementos(B, cardinalidadeB);
+        
+        System.out.println("União dos elementos do cojunto A e do conjunto B:");
+        conjunto.uniao(A, B, cardinalidadeA, cardinalidadeB);
+
+        System.out.println("Interseção dos elementos do conjunto A e do conjunto B:").
+        conjunto.intersecao(A, B, cardinalidadeA, cardinalidadeB);
+
+        System.out.println("Difença dos elementos do conjunto A e do conjunto B:");
+        conjunto.diferenca(A, B, cardinalidadeA,cardinalidadeB);
+
+        System.out.println("Produto cartesiano do conjunto A e do conjunto B:");
+        conjunto.produtoCartesiano(A, B, cardinalidadeA, cardinalidadeB);
+
+        System.out.println("Conjunto das Partes do conjunto A:");
+        conjunto.conjuntoDasPartes(A, cardinalidadeA);
+
+        System.out.println("Conjunto das Partes do conjunto B:");
+        conjunto.conjuntoDasPartes(B, cardinalidadeB);
     }
 }
